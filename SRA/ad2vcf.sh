e0c3a2bc09dc5529d4ee403bca6ebab7d2a985e5
Jason Bacon <bacon@barracuda.uits.uwm.edu>
2020-07-28 11:19:10 -0500

    Import SRA stuff from another repo

---
+++ SRA/ad2vcf.sh
@@ -0,0 +1,50 @@
+#!/bin/sh -e
+
+##########################################################################
+#   Script description:
+#       Process samples through ad2vcf
+#       
+#   History:
+#   Date        Name        Modification
+#   2020-07-27              Begin
+##########################################################################
+
+usage()
+{
+    printf "Usage: $0 sample-file\n"
+    exit 1
+}
+
+
+##########################################################################
+#   Main
+##########################################################################
+
+if [ $# != 1 ]; then
+    usage
+fi
+sample_file=$1
+
+#export PATH=./local/bin:$PATH
+which ad2vcf
+
+for sample in $(cat $sample_file); do
+    mount_dir=$(pwd)/mount-$sample
+    mkdir -p $mount_dir
+
+    srr=$(awk -v sample=$sample '$3 == sample { print $4 }' TOPMed_SraRunTable_20190628.txt)
+    printf "Sample: $sample  SRR: $srr\n"
+    fusera mount -t ../SRA/prj_13558_D25493.ngc -a $srr $mount_dir &
+    while [ ! -e $mount_dir/$srr/$sample.b38.irc.v1.cram ]; do
+	printf "Waiting for fusera...\n"
+	sleep 1
+    done
+    ls $mount_dir/$srr
+    
+    time samtools view -@ 2 --input-fmt-option required_fields=0x208 \
+	$mount_dir/$srr/$sample.b38.irc.v1.cram \
+	| ad2vcf combined.$sample.vcf.xz
+    
+    fusera unmount $mount_dir
+    rmdir $mount_dir
+done
