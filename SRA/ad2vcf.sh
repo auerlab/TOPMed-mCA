9518cad227b13df8af0a026817c26874ab56ca4c
Jason Bacon <bacon4000@gmail.com>
2020-07-29 00:46:40 +0000

    Get parallel-ad2vcf working cleanly

--- SRA/ad2vcf.sh
+++ SRA/ad2vcf.sh
@@ -11,7 +11,7 @@
 
 usage()
 {
-    printf "Usage: $0 sample-file\n"
+    printf "Usage: $0 sample-file VCF-dir\n"
     exit 1
 }
 
@@ -20,31 +20,40 @@ usage()
 #   Main
 ##########################################################################
 
-if [ $# != 1 ]; then
+if [ $# != 2 ]; then
     usage
 fi
 sample_file=$1
+vcf_dir=$2
 
 #export PATH=./local/bin:$PATH
 which ad2vcf
 
-for sample in $(cat $sample_file); do
-    mount_dir=$(pwd)/mount-$sample
-    mkdir -p $mount_dir
-
-    srr=$(awk -v sample=$sample '$3 == sample { print $4 }' TOPMed_SraRunTable_20190628.txt)
-    printf "Sample: $sample  SRR: $srr\n"
-    fusera mount -t Security/prj_13558_D25493.ngc -a $srr $mount_dir &
-    while [ ! -e $mount_dir/$srr/$sample.b38.irc.v1.cram ]; do
-	printf "Waiting for fusera...\n"
-	sleep 1
-    done
-    ls $mount_dir/$srr
+acc_list=TOPMed_SraRunTable_20190628.txt
+for sample in $(awk '{ print $1 }' $sample_file); do
+    study=$(awk -v sample=$sample '$3 == sample { print $2 }' $acc_list)
+    # Studies should have been filtered out before upload, but check again
+    if [ $study != phs000920 ] && [ $study != phs000921 ]; then
+	mount_dir=$(pwd)/mount-$sample
+	mkdir -p $mount_dir
     
-    time samtools view -@ 2 --input-fmt-option required_fields=0x208 \
-	$mount_dir/$srr/$sample.b38.irc.v1.cram \
-	| ad2vcf combined.$sample.vcf.xz
-    
-    fusera unmount $mount_dir
-    rmdir $mount_dir
+	srr=$(awk -v sample=$sample '$3 == sample { print $4 }' $acc_list)
+	printf "Sample: $sample  SRR: $srr\n"
+	fusera mount -t Security/prj_13558_D25493.ngc -a $srr $mount_dir &
+	while [ ! -e $mount_dir/$srr/$sample.b38.irc.v1.cram ]; do
+	    printf "Waiting for fusera...\n"
+	    sleep 1
+	done
+	ls $mount_dir/$srr
+	
+	time samtools view -@ 2 --input-fmt-option required_fields=0x208 \
+	    $mount_dir/$srr/$sample.b38.irc.v1.cram \
+	    | ad2vcf $vcf_dir/combined.$sample.vcf.xz
+	
+	fusera unmount $mount_dir
+	rmdir $mount_dir
+    else
+	printf "Hey! Found $study in $sample_file.\n"
+	printf "This should have been filtered out before uploading!\n"
+    fi
 done
