ae47a64d0a75beaeec2017291a70ec5ae178ae72
Jason Bacon <bacon@barracuda.uits.uwm.edu>
2020-09-11 09:32:52 -0500

    ad2vcf.sh: Validate output VCF and retry if corrupt

--- SRA/AWS-scripts/ad2vcf.sh
+++ SRA/AWS-scripts/ad2vcf.sh
@@ -33,41 +33,65 @@ which ad2vcf
 
 acc_list=TOPMed_SraRunTable_20200729.txt.xz
 for sample in $(awk '{ print $1 }' $sample_file); do
+    vcf_input=$vcf_dir/combined.$sample.vcf.xz
     # Some samples in acc_list are not in the BCFs
-    if [ ! -e $vcf_dir/combined.$sample.vcf.xz ]; then
+    if [ ! -e $vcf_input ]; then
 	printf "Missing VCF file: combined.$sample.vcf.xz\n"
     else
+	# phs000920 and phs000921 opted out of providing data for out study
+	# These should have been filtered out before upload, but check again
 	study=$(xzcat $acc_list | awk -v sample=$sample '$3 == sample { print $2 }')
-	# Studies should have been filtered out before upload, but check again
 	if [ $study != phs000920 ] && [ $study != phs000921 ]; then
-	    if [ -e $vcf_dir/Done/combined.$sample-ad.vcf.xz ]; then
+	    vcf_validated=$vcf_dir/Done/combined.$sample-ad.vcf.xz
+	    if [ -e $vcf_validated ]; then
 		printf "$sample already finished.\n"
 	    else
-		mount_dir=$(pwd)/mount-$sample
-		mkdir -p $mount_dir
-	    
-		srr=$(xzcat $acc_list | awk -v sample=$sample '$3 == sample { print $4 }')
-		printf "\n===================================================\n"
-		printf "Sample: $sample  SRR: $srr\n"
-		fusera mount -t Security/prj_13558_D25493.ngc -a $srr $mount_dir &
-		tries=0
-		while [ ! -e $mount_dir/$srr/$sample.b38.irc.v1.cram ] && [ $tries -lt 10 ]; do
-		    printf "Waiting for fusera...\n"
-		    sleep 3
-		    tries=$((tries + 1))
-		done
-		if [ tries = 10 ]; then
-		    printf "Giving up on $srr.\n"
-		else
-		    ls $mount_dir/$srr
-		    time samtools view -@ 2 --input-fmt-option required_fields=0x208 \
-			$mount_dir/$srr/$sample.b38.irc.v1.cram \
-			| ad2vcf $vcf_dir/combined.$sample.vcf.xz
-		    mv $vcf_dir/combined.$sample-ad.vcf.xz $vcf_dir/Done
+		# Retry ad2vcf command until we verify that the CRAM is still
+		# readable at the end and the VCF output is not corrupt
+		vcf_output=$vcf_dir/combined.$sample-ad.vcf.xz
+		while [ ! -e $vcf_validated ]; do
+		    mount_dir=$(pwd)/mount-$sample
+		    mkdir -p $mount_dir
+		
+		    srr=$(xzcat $acc_list | awk -v sample=$sample '$3 == sample { print $4 }')
+		    printf "\n===================================================\n"
+		    printf "Sample: $sample  SRR: $srr\n"
+		    fusera mount -t Security/prj_13558_D25493.ngc -a $srr $mount_dir &
 		    
-		    fusera unmount $mount_dir
-		    rmdir $mount_dir
-		fi
+		    # Verify that mount is complete before continuing
+		    tries=0
+		    while [ ! -e $mount_dir/$srr/$sample.b38.irc.v1.cram ] && \
+			  [ $tries -lt 10 ]; do
+			printf "Waiting for fusera...\n"
+			sleep 3
+			tries=$((tries + 1))
+		    done
+		    
+		    if [ tries = 10 ]; then
+			printf "Giving up on $srr.\n"
+		    else
+			ls $mount_dir/$srr
+			cram=$mount_dir/$srr/$sample.b38.irc.v1.cram
+			
+			# Can't rely on exit status since samtools is unhappy
+			# when ad2vcf closes the pipe.  Reading trailing junk
+			# until EOF in ad2vcf would waste a lot of time.
+			time samtools view -@ 2 \
+			    --input-fmt-option required_fields=0x208 \
+			    $cram | ad2vcf $vcf_niput || true
+			
+			# Check structure of last line and verify that fusera
+			# mount is still readable.   Mounts sometimes go bad,
+			# still showing files, but producing I/O errors
+			# on read.
+			if ./AWS-scripts/validate-vcf $vcf_output && \
+			    [ $(head -c 100 $cram | wc -c) = 100 ]; then
+			    mv $vcf_output $vcf_dir/Done
+			fi
+			fusera unmount $mount_dir
+			rmdir $mount_dir
+		    fi
+		done
 	    fi
 	else
 	    printf "Hey! Found $study in $sample_file.\n"
