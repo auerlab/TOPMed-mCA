b4e0c1fbfe8e6b369bc9184f2c788de6378c9c55
Jason Wayne Bacon <bacon@barracuda.uits.uwm.edu>
2021-12-04 11:35:49 -0600

    Clean up GSV VCF combine script and annex vcf2bed

--- 3d-filter-sites.sh
+++ 3d-filter-sites.sh
@@ -1,10 +1,16 @@
 #!/bin/sh -e
 
 ##########################################################################
-#   Script description:
-#       Filter sites for minimum allele frew (MAF) and minimum distance
-#       from each other
+#
+#   Filter sites for minimum allele frew (MAF) and minimum distance
+#   from each other
 #       
+#   All necessary tools are assumed to be in PATH.  If this is not
+#   the case, add whatever code is needed here to gain access.
+#   (Adding such code to your .bashrc or other startup script is
+#   generally a bad idea since it's too complicated to support
+#   every program with one environment.)
+#
 #   History:
 #   Date        Name        Modification
 #   2020-09-13  Jason Bacon Begin
