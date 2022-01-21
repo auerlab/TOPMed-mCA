1dbceae254f9a22e0a3bcc1e750d9db63a3a0294
Jason Bacon <bacon@head.albacore>
2022-01-21 10:22:28 -0600

    Import rerun.sh from CNC-EMDiff

---
+++ rerun.sh
@@ -0,0 +1,45 @@
+#!/bin/sh -e
+
+##########################################################################
+#   Description:
+#       Remove output files and logs from a previous run and resubmit
+#       
+#   History:
+#   Date        Name        Modification
+#   2021-11-27  Jason Bacon Begin
+##########################################################################
+
+usage()
+{
+    printf "Usage: $0 script.sbatch\n"
+    exit 1
+}
+
+
+##########################################################################
+#   Main
+##########################################################################
+
+if [ $# != 1 ]; then
+    usage
+fi
+script=$1
+
+base=${script%.sbatch}
+printf "Remove results from Data/$base? y/[n] "
+read sure
+if [ 0"$sure" = 0y ]; then
+    rm -rf Data/$base/*
+fi
+
+printf "Remove logs from Logs/$base? y/[n] "
+read sure
+if [ 0"$sure" = 0y ]; then
+    rm -rf Logs/$base/*
+fi
+
+if [ ${script##*.} = sbatch ]; then
+    echo sbatch $script
+else
+    echo ./$script
+fi
