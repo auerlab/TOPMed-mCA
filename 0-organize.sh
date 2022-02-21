641045b61b57a1490d6718408eaabe1e1fff8c35
Jason Bacon <bacon@head.albacore>
2022-02-21 09:22:24 -0600

    Add note to all sbatch scripts about program access/PATH

--- 0-organize.sh
+++ 0-organize.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-# Universal
 mkdir -p Data Logs
-for stage in 1-vcf-split 2-ad2vcf 3-filter 4-haplohseq 5-mocha; do
+scripts=$(ls [1-9]-*.s* [1-9][a-z]-*.s*)
+for script in $scripts; do
+    stage=${script%.*}
     mkdir -p Data/$stage Logs/$stage
 done
+ls Data
 
 # Local to UWM: Adjust to your site
 cd Data
-ln -sf ../../../SRR6990379/ Alignments
-ln -sf ../../../ncbi/dbGaP-13558/files/71699/topmed-dcc/exchange/phs000964_TOPMed_WGS_JHS/Combined_Study_Data/Genotypes/freeze.8/phased \
-    Raw-bcfs
 
 sample_id_file=samples.txt
 ls Alignments/*.cram | awk -F '[/\.]' '{ print $2 }' > $sample_id_file
