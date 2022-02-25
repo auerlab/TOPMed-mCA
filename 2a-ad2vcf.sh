6c9ed5b24e5332f16ff3d1da2c6b94264c2d61aa
Jason Bacon <bacon@head.albacore>
2022-02-24 18:33:44 -0600

    Minor shell fix

--- 2a-ad2vcf.sh
+++ 2a-ad2vcf.sh
@@ -3,4 +3,4 @@
 jobs=$(ls Data/1c-combine/*.vcf.xz | wc -l)
 jobs=$(echo $jobs)  # Remove leading space from wc
 printf "$jobs\n"
-sbatch 2a-ad2vcf.sbatch --array=1-$jobs%50
+sbatch --array=1-${jobs}%50 2a-ad2vcf.sbatch
