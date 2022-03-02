21c86649d9a04603e203504e8e4c22a74fd9c3a9
Jason Bacon <bacon@head.albacore>
2022-03-02 15:36:44 -0600

    Update and retest 5-mocha

--- 5-mocha.sh
+++ 5-mocha.sh
@@ -21,14 +21,14 @@ ref=GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
 site=https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids
 if [ ! -e Data/5-mocha/$ref ]; then
     cd Data/5-mocha
-    curl -C - -O $site/$ref.gz
-    gunzip $ref.gz
+    srun curl -C - -O $site/$ref.gz
+    srun gunzip $ref.gz
     cd ../..
 fi
 
 # Create index
 
-samples=$(cat Data/1-vcf-split/sample-list-all | wc -l)
+samples=$(cat Data/1c-combine/sample-list-all | wc -l)
 samples=$(echo $samples)
 set -x
 sbatch --array=1-$samples ./5-mocha.sbatch
